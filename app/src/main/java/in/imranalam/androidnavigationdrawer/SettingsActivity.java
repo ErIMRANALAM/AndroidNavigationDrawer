6e6880cddb01c807d90d1d1d190864feda6bc8e5
IMRAN ALAM <sr.imranalam@gmail.com>
2019-10-28 17:05:46 +0530

    Android Navigation Drawer Example using Activity v1

---
+++ app/src/main/java/in/imranalam/androidnavigationdrawer/SettingsActivity.java
@@ -0,0 +1,89 @@
+package in.imranalam.androidnavigationdrawer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class SettingsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    TextView mName, mEmail;
+    View mView;
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+///Nav Draw
+        Toolbar mToolbar = findViewById(R.id.iToolbar);
+        setSupportActionBar(mToolbar);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView mNavigationView = findViewById(R.id.nav_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
+        mView = mNavigationView.inflateHeaderView(R.layout.nav_header);
+        mName = mView.findViewById(R.id.hName);
+        mEmail = mView.findViewById(R.id.hEmail);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.nav_draw_open, R.string.nav_draw_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_a:
+                Intent m1 = new Intent(this, MainActivity.class);
+                startActivity(m1);
+                finish();
+                break;
+            case R.id.nav_b:
+                Intent m2 = new Intent(this, ContactsActivity.class);
+                startActivity(m2);
+                finish();
+                break;
+            case R.id.nav_c:
+//                Intent m3 = new Intent(this, SettingsActivity.class);
+//                startActivity(m3);
+                break;
+            case R.id.nav_d:
+                Intent m4 = new Intent(this, NotificationActivity.class);
+                startActivity(m4);
+                finish();
+                break;
+            case R.id.nav_e:
+                Intent mShare = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/dev?id=6492736305191471347"));
+                startActivity(mShare);
+                break;
+
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+}
